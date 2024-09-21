9fefe7f4ff079c65246073c17939be08566a5437
ImACaasje <141260210+ImACaasje@users.noreply.github.com>
2024-09-21 17:03:20 +0200

    bug fix
    bug fix

--- Interactable.gd
+++ Interactable.gd
@@ -2,26 +2,28 @@ extends Area2D
 
 @export var interaction_action_needed: bool = false
 @export var one_shot: bool = false
+@export var key_press_needed: Key = KEY_E
 
 var in_area: bool = false
 var paused: bool = false
 
-signal on_entered
-signal on_left
+signal on_entered(area)
+signal on_left(area)
 signal interaction
 
 func _ready() -> void:
 	$Text.visible = false
+	$Text.text = "Press " + OS.get_keycode_string(key_press_needed).to_upper()
 
 func _on_area_entered(area):
 	if !paused:
-		on_entered.emit()
+		on_entered.emit(area)
 		in_area = true
 		if interaction_action_needed: $Text.visible = true
 
 func _on_area_exited(area):
 	if !paused:
-		on_left.emit()
+		on_left.emit(area)
 		in_area = false
 		if interaction_action_needed: $Text.visible = false
 		if one_shot and !interaction_action_needed: queue_free()
@@ -29,6 +31,6 @@ func _on_area_exited(area):
 func _unhandled_input(event: InputEvent) -> void:
 	if !paused:
 		if in_area and interaction_action_needed:
-			if Input.is_action_just_pressed("interact"):
+			if Input.is_key_pressed(key_press_needed):
 				interaction.emit()
 				if one_shot: queue_free()
--- samurai_npc.gd
+++ samurai_npc.gd
@@ -4,13 +4,15 @@ extends Area2D
 
 
 func _on_interactable_interaction():
+	$AnimatedSprite2D.flip_h = $Interactable.get_overlapping_areas()[0].global_position.x < global_position.x
 	$Dialogue.start_dialogue()
 	$Interactable.paused = true
 
 
 #func _on_interactable_on_entered():
+	#$AnimatedSprite2D.flip_h = $Interactable.get_overlapping_areas()[0].global_position.x < global_position.x
 	#$Dialogue.start_dialogue()
-
+	#$Interactable.paused = true
 
 func _on_dialogue_on_dialogue_end():
 	$Interactable.paused = false
